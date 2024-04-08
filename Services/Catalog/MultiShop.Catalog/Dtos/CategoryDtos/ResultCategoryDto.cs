22097e90a08de9b89c9341c0193dcd079069c8ea
Ədalət <edaletbalaev@gmail.com>
2024-04-08 23:45:43 +0400

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+	public class ResultCategoryDto
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public String CategoryId { get; set; }
+		public String CategoryName { get; set; }
+	}
+}
